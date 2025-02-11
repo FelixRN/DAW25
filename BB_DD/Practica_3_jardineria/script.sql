3a6bbbac2987d7ab45c98f4da675c34842619a12
FelixRN <felixsr023@gmail.com>
2025-02-11 14:06:59 +0100

    edit

--- BB_DD/Practica_3_jardineria/script.sql
+++ BB_DD/Practica_3_jardineria/script.sql
@@ -1,62 +1,88 @@
--- La ciudad y el teléfono de las oficinas de Estados Unidos.
+# Solución de la práctica 4
 use jardineria;
-select * from Oficinas; 
-select Ciudad, Telefono from Oficinas where Pais LIKE "EEUU";
 
--- El nombre, los apellidos y el email de los empleados a cargo de Alberto Soria.
-select * from Empleados;
-select Nombre, Apellido1, Email from Empleados where (CodigoJefe = 3);
--- Mal ejecutado --> select Nombre, Apellido1, Apellido2, Email from Empleados where CodigoJefe like "3";
+-- Nombre del cliente, País, Fax y apellido del contacto de los clientes que han realizado pagos en el año 2008. Realizar sin JOIN, con INNER JOIN y con NATURAL JOIN
+select c.NombreCliente, c.Pais, c.Fax, c.ApellidoContacto 
+	from clientes as c, pagos as p where p.CodigoCliente = c.CodigoCliente 
+    and p.FechaPago like '%2008%'; 
+-- Vamos a realizar la misma Select pero con JOIN
+SELECT * FROM clientes INNER JOIN pagos ON clientes.CodigoCliente = pagos.CodigoCliente
+	where pagos.FechaPago like '%2008%'; # También funciona si quitamos el INNER
+-- Vamos a realizar la misma Select pero con NATURAL JOIN
+SELECT * FROM clientes NATURAL JOIN pagos 
+	where pagos.FechaPago like '%2008%'; # Se puede realizar porque el nombre de los campos son iguales.
 
-select Nombre, Apellido1, Apellido2, Email from Empleados where CodigoJefe = (select CodigoEmpleado from Empleados where Nombre like "Alberto" AND Apellido1 like "Soria");
+-- Listar todos los pedidos que realizó cada cliente
+select clientes.NombreCliente, clientes.ApellidoContacto, pedidos.CodigoPedido, pedidos.Estado 
+	from clientes, pedidos 
+    where clientes.CodigoCliente = pedidos.CodigoCliente;
+-- INNER JOIN
+select clientes.NombreCliente, clientes.ApellidoContacto, pedidos.CodigoPedido, pedidos.Estado FROM
+	clientes INNER JOIN pedidos ON clientes.CodigoCliente = pedidos.CodigoCliente;    
+-- NATURAL JOIN
+select clientes.NombreCliente, clientes.ApellidoContacto, pedidos.CodigoPedido, pedidos.Estado FROM
+	clientes NATURAL JOIN pedidos;
+    
+-- Misma consulta que antes pero utilzando alias de tabla    
+select c.NombreCliente as 'Nombre del cliente que realiza el pedido', c.ApellidoContacto, p.CodigoPedido, p.Estado 
+	from clientes as c, pedidos as p
+    where c.CodigoCliente = p.CodigoCliente;
+    
+     
+-- Nombre clientes y nombre de productos comprados
+SELECT c.nombreCliente, pr.Nombre
+FROM clientes as c, pedidos as p, detallePedidos as d, productos as pr 
+WHERE c.CodigoCliente = p.CodigoCliente 
+AND p.CodigoPedido = d.CodigoPedido
+AND d.CodigoProducto = pr.CodigoProducto;
+-- La misma consulta que la anterior pero utilizando INNER JOIN
+SELECT c.nombreCliente, pr.Nombre 
+FROM clientes AS c
+INNER JOIN pedidos AS p ON c.CodigoCliente = p.CodigoCliente
+INNER JOIN detallePedidos AS d ON p.CodigoPedido = d.CodigoPedido
+INNER JOIN productos AS pr ON d.CodigoProducto = pr.CodigoProducto;
+-- NATURAL JOIN
+SELECT c.nombreCliente, pr.Nombre 
+FROM clientes AS c
+NATURAL JOIN pedidos 
+NATURAL JOIN detallePedidos
+NATURAL JOIN productos AS pr;
 
--- El cargo, nombre, apellidos y email del jefe de la empresa.
-select Puesto, Nombre, Apellido1, Apellido2, Email from Empleados where CodigoJefe is null;
-
--- El nombre, apellidos y cargo de aquellos que no sean representantes de ventas.
-select * from empleados;
-select Nombre, Apellido1, Apellido2, Puesto from empleados where Puesto not like "Repesentate ventas";
-
--- El nombre de los clientes españoles.
-select NombreCliente from clientes where (Pais like "Spain%") or (Pais like "España%");
-
--- El código de empleado y el número de clientes al que atiende cada representante de ventas.
-select CodigoEmpleadoRepVentras, count(*) as num_clientes from clientes group by CodigoEmpleaddoRevVentas;
-
--- Cuál fue el primer y último pago que hizo algún cliente.
-select * from Pagos;
-
-select 
-
-
--- El código de cliente de aquellos clientes que hicieron pagos en 2008.
-
--- Los distintos estados por los que puede pasar un pedido.
-
--- El número de pedido, código de cliente, fecha requerida y fecha de entrega de los pedidos que no han sido entregados a tiempo.
-
--- -	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-		-	-	
--- Nombre del cliente, País, Fax y apellido del contacto de los clientes que han realizado pagos en el año 2008. Realizar sin JOIN, con =INNER JOIN y con NATURAL JOIN.)
-
--- Listar todos los pedidos que realizó cada cliente. Realizar sin JOIN, con INNER JOIN y con NATURAL JOIN.
-
--- Nombre clientes y nombre de productos comprados. Realizar sin JOIN, con INNER JOIN y con NATURAL JOIN..
 
 -- Seleccionar todos los pedidos que han realizado los clientes con el nombre 'Tendo%' Realizar la consulta con NATURAL JOIN e Inner Join
+-- INNER JOIN
+SELECT * FROM clientes INNER JOIN pedidos ON clientes.CodigoCliente = pedidos.CodigoCliente where clientes.NOmbreCliente like 'Tendo%';
+-- NATURAL JOIN
+SELECT * FROM clientes NATURAL JOIN pedidos where clientes.NOmbreCliente like 'Tendo%';
 
--- Realiza un CROSS JOIN para conocer el funcionamiento de este operador. Consulta todas las posibles combinaciones entre empleados y oficinas (tener en cuenta que no son combinaciones reales porque no se está teniendo en cuenta las tuplas relacionadas);
+-- Realiza un CROSS JOIN para conocer el funcionamiento de este operador. Consulta todas las posibles combinaciones entre empleados y oficinas (tener en cuenta que no son combinaciones reales porque no se está teniendo en cuenta las tuplas relacionadas)
+SELECT * FROM empleados CROSS JOIN oficinas;
 
--- El nombre, apellidos y cargo de aquellos que no sean representantes de ventas.
+-- El nombre, apellidos y cargo de aquellos que no sean representantes de ventas. 
+SELECT Nombre, Apellido1, Apellido2, Puesto FROM empleados WHERE Puesto not like '%Representante Ventas%'; -- Se trata de consulta de una tabla
 
 -- Muestra el nombre de los clientes que no hayan realizado pagos junto con el nombre de sus representantes de ventas y el código del cliente de la tabla pagos
+SELECT c.NombreCliente, e.Nombre, p.CodigoCliente FROM 
+	(clientes c INNER JOIN empleados e ON e.CodigoEmpleado = c.CodigoEmpleadoRepVentas) -- Unimos las dos tablas iniciales con un INNER JOIN, el resultado hacemos un left join para sacar tuplas de la tabla de la izquierda que cumplan que no tienen tuplas relacionadas en la tabla de la derecha
+    left outer join pagos p ON c.CodigoCliente = p.CodigoCliente
+    WHERE p.CodigoCliente is null; -- Si no ponemos esta condición salen los que tienen pagos y los que no tienen pagos.
 
 -- Muestra el nombre de los empleados que no estén asociados a ningún cliente (hacer un right o left join)
-
--- Muestra el nombre de los empleados que no estén asociados a ningún cliente (hacer un right o left join)
+SELECT * FROM clientes right outer join empleados ON clientes.CodigoEmpleadoRepVentas = empleados.CodigoEmpleado
+	WHERE clientes.CodigoEmpleadoRepVentas IS NULL; -- Si no ponemos esta opción salen los que están asociados a clientes y los que no
 
 -- Muestra todos los datos de clientes y pagos pero solo muestra los clientes que hayan realizado algún pago (hacer un right o left join)
+SELECT * FROM clientes left outer join pagos ON clientes.CodigoCliente = pagos.CodigoCliente
+	WHERE pagos.CodigoCliente IS NULL; -- Si no ponemos esta opción salen los que están asociados a clientes y los que no
 
 -- Muestra los empleados cuyo jefe directo sea Alberto con una consulta reflexiva
+SELECT * FROM empleados a inner join empleados b ON a.CodigoEmpleado = b.CodigoJefe
+	WHERE a.Nombre like 'Alberto'; -- Se hace un join de las dos tablas indicando la PK-FK y la condición
 
 -- Muestra el nombre de los clientes y su teléfono cuyos representantes de ventas sean Hilario y Lucio (con UNION)
+SELECT c.NombreCliente, c.Telefono, e.Nombre FROM clientes c, empleados e 
+	WHERE e.CodigoEmpleado = c.CodigoEmpleadoRepVentas AND e.Nombre like 'Mariano'
+UNION
+SELECT c.NombreCliente, c.Telefono, e.Nombre FROM clientes c, empleados e 
+	WHERE e.CodigoEmpleado = c.CodigoEmpleadoRepVentas AND e.Nombre like 'Lucio';
 
---
+++ BB_DD/Practica_5_uni/script.sql
@@ -0,0 +1,48 @@
+-- Devuelve un listado con el primer apellido, segundo apellido y el nombre de todos los alumnos. El listado deberá estar ordenado alfabéticamente de menor a mayor por el primer apellido, segundo apellido y nombre.
+select * from persona;
+select apellido1, apellido2, nombre from persona where tipo like "%alumno%" order by apellido1, apellido2, nombre;
+
+-- Averigua el nombre y los dos apellidos de los alumnos que no han dado de alta su número de teléfono en la base de datos.
+select * from persona;
+select nombre, apellido1, apellido2 from persona where tipo like "%alumno%" and telefono is null; 
+
+-- Devuelve el listado de los alumnos que nacieron en 1999.
+select * from persona where tipo like "%alumno%" and fecha_nacimiento = 1999;
+
+-- Devuelve el listado de profesores que no han dado de alta su número de teléfono en la base de datos y además su nif termina en K.
+select nombre, apellido1, apellido2 from persona where tipo like "%profesor%" and telefono is null and nif like "%k";
+
+-- Devuelve el listado de las asignaturas que se imparten en el primer cuatrimestre, en el tercer curso del grado que tiene el identificador 7.
+select * from asignatura where cuatrimestre = 1  and curso = 3 and id_grado = 7;
+
+-- Consultas multitabla (Composición interna)
+
+-- 6. Devuelve un listado con los datos de todas las alumnas que se han matriculado alguna vez en el Grado en Ingeniería Informática (Plan 2015).
+select * from grado;
+select * from persona;
+
+select * from persona p INNER JOIN alumno_se_matricula_asignatura am on  p.id = am.id_alumno
+inner join asignatura a on am.id_asignatura = a.id
+inner join grado g on a.id_grado = g.id 
+where p.sexo like "%M%" and g.id = 4;
+
+-- 7.Devuelve un listado con todas las asignaturas ofertadas en el Grado en Ingeniería Informática (Plan 2015).
+select * from asignatura;
+select * from asignatura a INNER JOIN grado g on g.id = a.id_grado
+where g.id = 4;
+
+-- 8.Devuelve un listado de los profesores junto con el nombre del departamento al que están vinculados. El listado debe devolver cuatro columnas, 
+-- primer apellido, segundo apellido, nombre y nombre del departamento. El resultado estará ordenado alfabéticamente de menor a mayor por los apellidos y el nombre.
+select * from profesor;
+select * from departamento;
+select * from persona;
+
+select * from profesor pro INNER JOIN departamento d on d.id =  pro.id_departamento 
+inner join persona p on  p.tipo = pro.id;
+where 
+
+-- 9. Devuelve un listado con el nombre de las asignaturas, año de inicio y año de fin del curso escolar del alumno con nif 26902806M.
+
+-- 10. Devuelve un listado con el nombre de todos los departamentos que tienen profesores que imparten alguna asignatura en el Grado en Ingeniería Informática (Plan 2015).
+
+-- 11. Devuelve un listado con todos los alumnos que se han matriculado en alguna asignatura durante el curso escolar 2018/2019.
