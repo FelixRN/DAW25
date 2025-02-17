5fea7db33e07264903009878f1bac601993d95ac
FelixRN <felixsr023@gmail.com>
2025-02-17 14:14:03 +0100

    autocommit

---
+++ LL.MM/3_Eva/Scripts/script.js
@@ -0,0 +1,23 @@
+alert("¡Hola Mundo 1!");
+
+function gestionarUsuario() {
+    let nombre = prompt("Introduzca su nombre, por favor");
+
+    switch (nombre = null) {
+        case 1:
+            console.warn("Que introduzcas tu nombre" )
+            break;
+    
+        case 2:
+            console.alert("NO HAS ESCRITO TU NOMBRE")
+            break;
+
+        case 3:
+            console.alert("NO HAS ESCRITO TU NOMBRE")
+            break;
+    }
+
+}
+function confirmacion() {
+ confirm("Confirmar");
+}
