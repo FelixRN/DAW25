3bf5407ca9bd006674f64d99105f003d30596092
Felix <felixsr023@gmail.com>
2025-03-17 12:06:47 +0100

    js

---
+++ correccion_2.js
@@ -0,0 +1,25 @@
+function ejercicio1() {
+    let N = parseInt(prompt("Introduce un número: "));;
+
+    let arrayPersonas = [];
+
+    for (let i = 0; i < N; i++) {
+        let input = prompt("Introduce " + (i+1) + " nombres: " );
+        arrayPersonas.push(input);
+}
+
+    console.log(arrayPersonas);
+
+    //Mientras no quende nombres en el array // while (arrayPersonas != 0)
+while (arrayPersonas.length) {
+    var P = parseInt(prompt("Introduce un número entre 0 y " + N + ":" ));//Preguntar
+
+    if (P > 0 ) {
+        arrayPersonas.splice(P,1)
+    } else {
+        
+    }
+}
+
+
+}
